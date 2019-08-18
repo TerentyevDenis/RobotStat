2fc49072a51ae331716d3b11a53f862d8b806ec7
TerentevDenis <terentyevdenis7@gmail.com>
2019-08-18 18:38:55 +0300

    add build table

--- src/main/kotlin/ru/terentev/Controllers/DBHelper.kt
+++ src/main/kotlin/ru/terentev/Controllers/DBHelper.kt
@@ -30,12 +30,18 @@ class DBHelper(){
         val COLUMN_TIME = "starttime"
         val COLUMN_DURATION = "duration"
         val COLUMN_RESULT = "testresult"
+        val COLUMN_BUILDID = "build_id"
+        val TABLE_BUILD = "builds"
+        val COLUMN_BUILD_ID = "_id"
+        val COLUMN_JENKINS_ID = "jenkins_id"
+        val COLUMN_ADD_DATE = "add_date"
     }
 
     fun opendb(exec:(Connection)->Unit){
         val url = "jdbc:sqlite:$DATABASE_NAME"
         try {
             DriverManager.getConnection(url).use { conn ->
+                   conn.createStatement().execute("PRAGMA foreign_keys = ON")
                    exec(conn)
             }
         } catch (e: SQLException) {
@@ -43,40 +49,51 @@ class DBHelper(){
         }
     }
 
-    fun createTable(){
+    fun createTable() {
         opendb { conn ->
             conn.createStatement().execute(
-                "CREATE TABLE IF NOT EXISTS $TABLE_NAME (\n"+
-                        "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT,\n"+
-                        "$COLUMN_NAME TEXT NOT NULL UNIQUE);\n")
+                    "CREATE TABLE IF NOT EXISTS $TABLE_NAME (\n" +
+                            "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                            "$COLUMN_NAME TEXT NOT NULL UNIQUE);\n")
+            conn.createStatement().execute(
+                    "CREATE TABLE IF NOT EXISTS $TABLE_BUILD (\n" +
+                            "$COLUMN_BUILD_ID INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                            "$COLUMN_JENKINS_ID INTEGER,\n"+
+                            "$COLUMN_ADD_DATE TEXT NOT NULL);\n")
 
-            conn.createStatement().execute( "CREATE TABLE IF NOT EXISTS $TABLE_TEST (\n"+
-                "$COLUMN_RESULT_ID INTEGER PRIMARY KEY AUTOINCREMENT,\n"+
-                "$COLUMN_TEST_ID INTEGER NOT NULL,\n" +
-                "$COLUMN_TIME TEXT NOT NULL,\n"+
-                "$COLUMN_DURATION REAL NOT NULL,\n"+
-                "$COLUMN_RESULT TEXT NOT NULL,\n"+
-                "FOREIGN KEY ($COLUMN_TEST_ID) REFERENCES $TABLE_NAME($COLUMN_ID));")}
+            conn.createStatement().execute("CREATE TABLE IF NOT EXISTS $TABLE_TEST (\n" +
+                    "$COLUMN_RESULT_ID INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                    "$COLUMN_TEST_ID INTEGER NOT NULL,\n" +
+                    "$COLUMN_TIME TEXT NOT NULL,\n" +
+                    "$COLUMN_DURATION REAL NOT NULL,\n" +
+                    "$COLUMN_RESULT TEXT NOT NULL,\n" +
+                    "$COLUMN_BUILDID INTEGER NOT NULL,\n" +
+                    "FOREIGN KEY ($COLUMN_TEST_ID) REFERENCES $TABLE_NAME($COLUMN_ID) ON DELETE CASCADE,\n" +
+                    "FOREIGN KEY ($COLUMN_BUILDID) REFERENCES $TABLE_BUILD($COLUMN_BUILD_ID) ON DELETE CASCADE);")
+        }
     }
 
     fun putListInDB(list: ArrayList<Test>){
         runAsync (statusBar){
             updateMessage("Loading results...")
             opendb { conn ->
+                conn.createStatement().execute("INSERT INTO $TABLE_BUILD($COLUMN_ADD_DATE) VALUES (CURRENT_TIMESTAMP)")
+                var buildIDqury = conn.createStatement().executeQuery("SELECT $COLUMN_BUILD_ID from $TABLE_BUILD WHERE $COLUMN_BUILD_ID = last_insert_rowid()")
+                var buildID = buildIDqury.getInt("_id")
                 for ((i,test) in list.withIndex()) {
                     updateProgress(i.toDouble(), list.size.toDouble())
                     if (!conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY).executeQuery(
-                                    "SELECT _id from tests WHERE testname='" + test.name + "';").next()) {
-                        conn.createStatement().execute("INSERT INTO tests(testname) VALUES ('" + test.name + "');")
+                                    "SELECT _id from $TABLE_NAME WHERE $COLUMN_NAME='" + test.name + "';").next()) {
+                        conn.createStatement().execute("INSERT INTO $TABLE_NAME($COLUMN_NAME) VALUES ('" + test.name + "');")
                     }
-                    var rs = conn.createStatement().executeQuery("SELECT _id from tests WHERE testname='" + test.name + "';")
+                    var rs = conn.createStatement().executeQuery("SELECT _id from $TABLE_NAME WHERE $COLUMN_NAME='" + test.name + "';")
                     rs.next()
                     var test_id = rs.getInt("_id")
                     rs.close()
                     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
-                    conn.createStatement().execute("INSERT INTO tests_duration(test_id, starttime, duration, testresult) " +
+                    conn.createStatement().execute("INSERT INTO tests_duration(test_id, starttime, duration, testresult, build_id) " +
                             "VALUES (" + test_id + ", strftime('%Y-%m-%d %H:%M:%f','" + formatter.format(test.start) + "'), "
-                            + test.time + ", '" + test.status?.name + "');")
+                            + test.time + ", '" + test.status?.name + "', " +buildID +");")
                 }
             }
             updateMessage("Updating table...")
